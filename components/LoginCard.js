e61b62206abcbf1a82baf90afed17ee06b15d7dc
zyllar68 <zyrilmancion@gmail.com>
2023-02-26 02:58:14 +0800

    update login card component

--- components/LoginCard.js
+++ components/LoginCard.js
@@ -1,11 +1,14 @@
-import { Input } from "@/components";
+import { Input, Button } from "@/components";
 
 const LoginCard = () => {
   return (
     <div className='loginCard'>
       <h1>Login</h1>
       <form>
-        <Input />
+        <Input marginTop='31px' placeholder='Username' />
+        <Input marginTop='1.25rem' placeholder='password' type='password' />
+        <p>error message</p>
+        <Button marginTop='1.875rem' title='Login' primary />
       </form>
     </div>
   );
