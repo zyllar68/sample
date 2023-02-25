ae15920a8e0a928815b530292c7999dc7f9424d2
zyllar68 <zyrilmancion@gmail.com>
2023-02-26 02:18:18 +0800

    added login card components

---
+++ components/LoginCard.js
@@ -0,0 +1,14 @@
+import { Input } from "@/components";
+
+const LoginCard = () => {
+  return (
+    <div className='loginCard'>
+      <h1>Login</h1>
+      <form>
+        <Input />
+      </form>
+    </div>
+  );
+};
+
+export default LoginCard;
