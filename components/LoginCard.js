ff86d499545a5c7f657b5e70f31fb7f3076e014e
zyllar68 <zyrilmancion@gmail.com>
2023-02-26 14:05:44 +0800

    added temporary route to mobile landing page

--- components/LoginCard.js
+++ components/LoginCard.js
@@ -1,10 +1,18 @@
 import { Input, Button } from "@/components";
+import { useRouter } from "next/router";
 
 const LoginCard = () => {
+  const router = useRouter();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    router.push("/mobile");
+  };
+
   return (
     <div className='loginCard'>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <Input marginTop='31px' placeholder='Username' />
         <Input marginTop='1.25rem' placeholder='password' type='password' />
         <p>error message</p>
