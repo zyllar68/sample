159a4fe69195c17233db0481278e893ffcfa47c6
zyllar68 <zyrilmancion@gmail.com>
2023-02-27 00:24:21 +0800

    exported webnavbar component

--- components/index.js
+++ components/index.js
@@ -3,3 +3,4 @@ export { default as Input } from "./Input";
 export { default as Button } from "./Button";
 export { default as MobileNavbar } from "./MobileNavbar";
 export { default as Table } from "./Table";
+export { default as WebNavbar } from "./WebNavbar";
