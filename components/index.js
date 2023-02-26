edfc5c1f48af0375d6db148ca511f38e704c10ba
zyllar68 <zyrilmancion@gmail.com>
2023-02-26 12:55:12 +0800

    added new components in exports

--- components/index.js
+++ components/index.js
@@ -1,3 +1,5 @@
 export { default as LoginCard } from "./LoginCard";
 export { default as Input } from "./Input";
 export { default as Button } from "./Button";
+export { default as MobileNavbar } from "./MobileNavbar";
+export { default as Table } from "./Table";
