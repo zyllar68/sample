cf0018bdf6d129a5a5b7f4237c934e996488cade
Zyril Mancion <zyrilmancion@gmail.com>
2023-03-01 22:46:16 +0800

    testing update

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,22 +1,5 @@
-import clientPromise from "@/lib/mongodb";
-
-const NEXT_API_KEY = process.env.NEXT_API_KEY;
-
 export default async function handler(req, res) {
-  const apiKey = req.headers["api-key"];
   const ipAddress = req.headers["x-real-ip"];
-  const client = await clientPromise;
-  const db = client.db(process.env.MONGODB_NAME);
 
-  if (NEXT_API_KEY !== apiKey) {
-    return res.status(401).json({ message: apiKey });
-  }
-
-  if (req.method === "POST") {
-    try {
-      res.status(200).json(`yeah ${ipAddress}`);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  res.status(200).json(`yeah ${ipAddress}`);
 }
